2bee51ef5244522ade72bfbfe520f51569e43945
Sandeep <sandeepdewangan7@gmail.com>
2025-02-06 23:22:04 +0530

    useRef -> manipulating dom elements

--- readme.md
+++ readme.md
@@ -9,3 +9,11 @@
 Dependency changes (eg while searching title changes)
 
 `Re-Render -> Commit -> Browser Paint -> Effect -> Unmount`
+
+### Important
+
+- Ref are persistent among the renders.
+- Ref are used to select and store the DOM elements.
+- Ref are not rendered in JSX.
+- Ref are not re-rendered on update.
+- Ref are used inside useEffect()
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
+import { useEffect, useRef } from "react";
+
 export default function Header({ query, setQuery }) {
+  const inputEl = useRef(null); // null for events
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
+
   return (
     <>
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     </>
   );
